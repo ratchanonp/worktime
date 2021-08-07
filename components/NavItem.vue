df53c07d2655a35e45014c7df04f82021d8f2b49
ratchanonp <qq.panmas@gmail.com>
2021-08-07 21:19:22 +0700

    ✏️ Menu use DaisyUI

--- components/NavItem.vue
+++ components/NavItem.vue
@@ -1,36 +1,3 @@
-<<<<<<< HEAD
-<template>
-	<li class="nav-item px-4 py-2.5 hover:bg-green-500 rounded">
-		<NuxtLink class="nav-link flex flex-row" :to="to">
-			<div class="w-8 mr-2">
-				<font-awesome-icon :icon="icon" class="text-xl" />
-			</div>
-			<div>
-				<span class="nav-link-text">{{ title }}</span>
-			</div>
-		</NuxtLink>
-	</li>
-</template>
-
-<script>
-export default {
-	props: {
-		to: {
-			type: String,
-			required: true,
-		},
-		title: {
-			type: String,
-			required: true,
-		},
-		icon: {
-			type: String,
-			required: true,
-		},
-	},
-};
-</script>
-=======
 <template>
 	<li>
 		<NuxtLink :to="to" class="flex ">
@@ -68,4 +35,3 @@ export default {
 		background-color: #40BF6C !important;
 	}
 </style>
->>>>>>> main
