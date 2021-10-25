0dc02a6a06b0551c72a27d3030ed7a7309f2985b
ratchanonp <qq.panmas@gmail.com>
2021-10-25 23:50:14 +0700

    :zap: Close System

---
+++ pages/break.vue
@@ -0,0 +1,31 @@
+<template>
+	<div class="flex min-h-screen font-prompt mx-auto justify-center">
+			<div class="flex flex-col my-auto">
+				<div class="title mb-10">
+					<img class="w-32 h-32" src="logo.png" alt="" />
+					<h1 class="text-2xl md:text-4xl font-bold text-black">
+						ระบบลงเวลาปฏิบัติราชการ
+					</h1>
+					<h2 class="text-1xl md:text-2xl text-primary">
+						โรงเรียนสินปุญคุณวิชญ์
+					</h2>
+				</div>
+				<div class="bg-white p-5 w-1/2 rounded-xl shadow-xl w-full mb-5">
+					<LoginForm />
+				</div>
+				<small class="text-white"
+					>&copy; Ratchanon Panmas</small
+				>
+			</div>
+	</div>
+</template>
+
+<script>
+export default {
+	head() {
+		return {
+			title: "เข้าสู่ระบบ",
+		};
+	},
+};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,31 +1,10 @@
 <template>
-	<div class="flex min-h-screen font-prompt mx-auto justify-center">
-			<div class="flex flex-col my-auto">
-				<div class="title mb-10">
-					<img class="w-32 h-32" src="logo.png" alt="" />
-					<h1 class="text-2xl md:text-4xl font-bold text-black">
-						ระบบลงเวลาปฏิบัติราชการ
-					</h1>
-					<h2 class="text-1xl md:text-2xl text-primary">
-						โรงเรียนสินปุญคุณวิชญ์
-					</h2>
-				</div>
-				<div class="bg-white p-5 w-1/2 rounded-xl shadow-xl w-full mb-5">
-					<LoginForm />
-				</div>
-				<small class="text-white"
-					>&copy; Ratchanon Panmas</small
-				>
-			</div>
-	</div>
+    <div class="flex min-h-screen justify-center font-prompt">
+        <div class="flex-col text-center my-auto">
+            <img src="6873-under-maintenance.gif" class="w-64 h-64 mb-20">
+            <h1 class="text-4xl md:text-7xl">ปิดระบบ</h1>
+            <p class="text-md md:text-xl">กำลังสรุปผลการลงเวลาปฏิบัติราชการ</p>
+        </div>
+    </div>
 </template>
 
-<script>
-export default {
-	head() {
-		return {
-			title: "เข้าสู่ระบบ",
-		};
-	},
-};
-</script>
